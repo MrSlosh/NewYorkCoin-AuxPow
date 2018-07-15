53cae90062630899424f79682442037658afbe47
MrSlosh <jason@blackboxrealities.com>
2018-07-15 08:20:20 -0500

    adding debug statements, reverting test

--- src/pow.cpp
+++ src/pow.cpp
@@ -91,6 +91,7 @@ uint32_t KimotoGravityWell(const CBlockIndex* pindexLast, const CBlockHeader *pb
 
   	arith_uint256 bnNew(PastDifficultyAverage);
   	if (PastRateActualSeconds != 0 && PastRateTargetSeconds != 0) {
+      LogPrintf("multiplying by pastrateactualseconds and dividing by pastratetargetseconds");
   		bnNew *= PastRateActualSeconds;
   		bnNew /= PastRateTargetSeconds;
   	}
