fc22b8da7871498ff4f6b2aa13e83d6d9ac51bdf
MrSlosh <jason@blackboxrealities.com>
2018-07-16 16:24:03 -0500

    more fixes for pow.cpp

--- src/pow.cpp
+++ src/pow.cpp
@@ -53,7 +53,7 @@ uint32_t KimotoGravityWell(const CBlockIndex* pindexLast, const CBlockHeader *pb
       return bnProofOfWorkLimit;
     }
 
-  	if (BlockLastSolved == NULL || BlockLastSolved->nHeight == 0 || (uint64_t)BlockLastSolved->nHeight < PastBlocksMin) { return bnProofOfWorkLimit; }
+  	if (BlockLastSolved == NULL || BlockLastSolved->nHeight == 0 || (uint64_t)BlockLastSolved->nHeight < PastBlocksMin) { return UintToArith256(params.powLimit).GetCompact(); }
 
   	for (uint32_t i = 1; BlockReading && BlockReading->nHeight > 0; i++) {
               if (PastBlocksMax > 0 && i > PastBlocksMax) { break; }
